79182c4c9b6fff763a349b6e7e4e3a0555fc7a97
Poloz Alexey <polozhev@mail.ru>
2017-04-29 14:57:29 +0300

    site fixes

--- site/load.py
+++ site/load.py
@@ -1,13 +1,17 @@
-import sys, ssl, chardet
+import codecs, sys, ssl, chardet
 if sys.version_info[0]>=3:
 	from urllib.request import urlopen
 else:
 	from urllib import urlopen
 
+db='db.uple'
+
 def get(src):
 	context=ssl._create_unverified_context()
 	with urlopen(src,context=context) as site:
 		text=site.read()
-		return text.decode(chardet.detect(text)['encoding'])
+		with codecs.open(db,'w','utf-8') as file:
+			print(text.decode(chardet.detect(text)['encoding']),file=file)
 
-print(get(input()))
+get(input())
+print('OK')
--- ssl/ssl.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-error_reporting(E_ALL);
-$file = file_get_contents('https://www.google.com/');
-die($file);
-
-?>
