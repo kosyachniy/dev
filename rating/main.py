d4882c643048f89d039f445a771551f52c207cbd
Poloz Alexey <polozhev@mail.ru>
2017-05-13 13:25:59 +0300

    Changed structure of rating

--- rating/main.py
+++ rating/main.py
@@ -3,12 +3,26 @@ with open('db.txt','r') as file:
 	for i in file:
 		a.append(i.strip())
 
-for i in range(len(a)-1):
-	print(a[i],' | ',a[i+1])
+b=input()
+interval=len(a)
+shift=0
+while interval>1:
+	if interval%2:
+		interval=(interval//2)+1
+	else:
+		interval//=2
+	contr=interval+shift-1
+	if contr>=len(a):
+		break
+	c=a[contr]
+	print(c,' | ',b)
 	r=input()
-	while r!=a[i] and r!=a[i+1]:
+	while r!=c and r!=b:
 		r=input()
-	if a[i+1]==r:
-		a[i],a[i+1]=a[i+1],a[i]
+	if r==c:
+		shift+=interval
 
+a.append(b)
+for i in range(shift+1,len(a))[::-1]:
+	a[i],a[i-1]=a[i-1],a[i]
 print(a)
