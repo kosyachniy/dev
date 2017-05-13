9dbdef5fb9471db2623000fcddb3ffd59e358bc8
Poloz Alexey <polozhev@mail.ru>
2017-05-13 18:31:57 +0300

    All bug fixed

--- rating/main.py
+++ rating/main.py
@@ -6,48 +6,31 @@ with open('db.txt','r') as file:
 b=input()
 interval=len(a)
 shift=0
-c=None
 while interval>=1:
-	if interval%2:
-		i=(interval//2)+shift+1
-	else:
-		i=interval//2+shift
-	'''
 	t=interval%2
 	if t:
-		interval+=1
-	interval//=2
-	contr=interval+shift-1
-	if contr>=len(a) or c==a[contr]:
-		break
-	c=a[contr]
-	print(interval,shift,contr)
-	'''
+		i=(interval//2)+1
+	else:
+		i=interval//2
 	if i>len(a):
 		break
-	c=a[i-1]
-	print(interval,shift,i)
+	c=a[i+shift-1]
 	print('1 - ',c,'  |  2 - ',b)
 	r=input()
 	while r!='1' and r!='2':
 		r=input()
+	interval//=2
 	if r=='1':
-		interval//=2
 		shift+=i
 	else:
-		if interval%2:
-			interval//=2
-		else:
-			interval=(interval//2)-1
+		if not t:
+			interval-=1
 
 a.append(b)
 for i in range(shift+1,len(a))[::-1]:
 	a[i],a[i-1]=a[i-1],a[i]
 
-print(a)
-'''
 with open('db.txt','w') as file:
 	for i in range(len(a)-1):
 		print(a[i],file=file)
 	print(a[len(a)-1],end='',file=file)
-'''
