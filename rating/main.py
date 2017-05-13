8ccb3edee08c4cbe8a204023abab4d03d453bc37
Poloz Alexey <polozhev@mail.ru>
2017-05-13 16:01:57 +0300

    Positive add elements

--- rating/main.py
+++ rating/main.py
@@ -3,6 +3,7 @@ with open('db.txt','r') as file:
 	for i in file:
 		a.append(i.strip())
 
+a=a[::-1] #!Для позитивного добавления (в плюс меньше дейстий, чем в минус)
 b=input()
 interval=len(a)
 shift=0
@@ -19,10 +20,11 @@ while interval>1:
 	r=input()
 	while r!=c and r!=b:
 		r=input()
-	if r==c:
+	if r==b: #!
 		shift+=interval
 
 a.append(b)
 for i in range(shift+1,len(a))[::-1]:
 	a[i],a[i-1]=a[i-1],a[i]
+a=a[::-1] #!
 print(a)
