1c83ae7ca7e4ce42c0837d82d0b0f43aee13ac88
Poloz Alexey <polozhev@mail.ru>
2017-05-13 16:38:24 +0300

    Error with dublicate question

--- rating/main.py
+++ rating/main.py
@@ -3,28 +3,36 @@ with open('db.txt','r') as file:
 	for i in file:
 		a.append(i.strip())
 
-a=a[::-1] #!Для позитивного добавления (в плюс меньше дейстий, чем в минус)
 b=input()
 interval=len(a)
 shift=0
-while interval>1:
+c=None
+while interval>=1:
+	print(interval,end=' ')
 	if interval%2:
-		interval=(interval//2)+1
+		t=True
 	else:
-		interval//=2
+		t=False
+	interval//=2
 	contr=interval+shift-1
+	if t:
+		contr+=1
 	if contr>=len(a):
+		break
+	print(shift,contr)
+	if c==a[contr]:
 		break
 	c=a[contr]
 	print(c,' | ',b)
 	r=input()
 	while r!=c and r!=b:
 		r=input()
-	if r==b: #!
+	if r==c:
 		shift+=interval
+		if t:
+			shift+=1
 
 a.append(b)
 for i in range(shift+1,len(a))[::-1]:
 	a[i],a[i-1]=a[i-1],a[i]
-a=a[::-1] #!
 print(a)
