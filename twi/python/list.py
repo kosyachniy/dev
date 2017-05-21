052d75d508ce141fcd2b0b2f5fd783e858763892
Poloz Alexey <polozhev@mail.ru>
2017-05-21 15:07:04 +0300

    List of new subscribe in twitter

--- twi/python/list.py
+++ twi/python/list.py
@@ -3,7 +3,8 @@ import time
 
 x=input()
 for i in api.followers(x):
-	print(i.screen_name)
+	if i.friends_count>=0.6*i.followers_count:
+		print(i.followers_count,i.friends_count,i.screen_name)
 '''
 api.get_user(x).follow()
 y=api.show_friendship(source_screen_name=x,target_screen_name='kosyachniy')[0]
--- twi/python/text.py
+++ twi/python/text.py
@@ -1,19 +1,10 @@
-import tweepy, sys, os, ssl, chardet
+import sys, os, ssl, chardet
 if sys.version_info[0]>=3:
 	from urllib.request import urlretrieve, urlopen
 else:
 	from urllib import urlretrieve, urlopen
+from func import *
 
-consumer_key='8Y2o5PjasQkVmvoxVQBLyVs4F'
-consumer_secret='2r4KPBm8kCcKsNrwkzSQRH4IkDqbxhVrgVcAmvfoyfXbZUNm1L'
-access_key='3110781773-4I27iEchYIxqZmIQOgt18b2ehZUHkdEpWKRPuRO'
-access_secret='OZj9w6TRUQoeVCgk3pShYOTFWasvSx3ebgOXn6lJQyngy'
-'''
-consumer_key='jARMeUTgjWhDSWOSbs0pfQYz4'
-consumer_secret='stRnOnA3KRpaCI3hjyJyo0eLedQNflQ4FuvudvXj37mSLX7tPa'
-access_key='4100776272-4VpvJNSkyhG9kWXfhRHyI3eTEeBRYRABiIqzusU'
-access_secret='RHcdHHf0TyHBxtfxHyRCgmB41r7c6gnVgUHH7SEf1Uqi9'
-'''
 url='http://t30p.ru/'
 adr='http://api.forismatic.com/api/1.0/?method=getQuote&format=text&language=ru'
 contain='Тренды твиттера'
@@ -44,17 +35,8 @@ def cont():
 def post(text):
 	if not text:
 		text=cont()
-#Подключение
-	auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
-	auth.set_access_token(access_key,access_secret)
-	api=tweepy.API(auth)
-#Отправка текстового твита
-	api.update_status(text)
-
+	api.update_status(text) #Отправка текстового твита
 	return text
 
 if __name__=='__main__':
 	post('')
-
-#Получение логина и имени юзера | print(api.me().screen_name, api.me().name)
-#Фоловим другого юзера | api.get_user('muzhig').follow()
