65c946214f4038d628c454fc93f1fbe23a0b0bee
Poloz Alexey <polozhev@mail.ru>
2017-07-09 12:45:01 +0500

    Bot

---
+++ bot/telegram/main.py
@@ -0,0 +1,15 @@
+import telebot
+
+token='417063852:AAFvfJdVGgLv9odlnY_gaiMmV4NIBMlgvOQ'
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+	bot.send_message(message.chat.id, 'Hi!')
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+	bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
