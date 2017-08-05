bf04e62ff63f81e38bb2f00aac0ff1faf8597e31
Poloz Alexey <polozhev@mail.ru>
2017-08-05 14:19:36 +0300

    Worked upload images

--- bot/vk/func.py
+++ bot/vk/func.py
@@ -1,38 +1,32 @@
-import vk_api, time, json
+import time, requests, vk_api, json
 
 with open('set.txt', 'r') as file:
 	s=json.loads(file.read())
-	login=s['login']
-	password=s['password']
 
 	vk=vk_api.VkApi(token=s['token'])
-	#vk=vk_api.VkApi(login='', password='')
-
 	vk.auth()
 
-def send(user, cont, img=[]):
-	vks=vk_api.VkApi(login=login, password=password)
+	vks=vk_api.VkApi(login=s['login'], password=s['password'])
 	vks.auth()
 
-	for i in range(img):
+def send(user, cont, img=[]):
+	for i in range(len(img)):
 		if img[i][0:5]!='photo':
 #Загружаем изображение на сервер
-			out=open('re.jpg', 'wb')
-			out.write(requests.get(img[i]).content)
-			out.close()
+			file=open('re.jpg', 'wb')
+			file.write(requests.get(img[i]).content)
+			file.close()
 
 #Загружаем изображение в ВК
 			upload=vk_api.VkUpload(vks)
-			photo=upload.photo('re.jpg', album_id=247265476, group_id=151412216)[0]
+			photo=upload.photo('re.jpg', group_id=151412216, album_id=247265476)[0]
 			img[i]='photo{}_{}'.format(photo['owner_id'], photo['id'])
 
 	return vk.method('messages.send', {'user_id':user, 'message':cont, 'attachment':','.join(img)})
 
-def read():
-	cont=[]
-	for i in vk.method('messages.get')['items']:
-		if not i['read_state']:
-			cont.append([i['user_id'], i['body']])
+read=lambda:
+#Ищем непрочитанные
+	cont=[[i['user_id'], i['body']] for i in vk.method('messages.get')['items'] if not i['read_state']]
 	return cont[::-1]
 
 dial=lambda: [i['message']['user_id'] for i in vk.method('messages.getDialogs')['items']]
@@ -41,6 +35,7 @@ def info(user):
 	x=vk.method('users.get', {'user_ids': user, 'fields': 'verified, first_name, last_name, sex, bdate, photo_id, country, city, home_town, screen_name'})[0]
 	#, lang, phone, timezone
 
+#Форматируем дату
 	bd=x.get('bdate').count('.')
 	if bd==2:
 		bd=time.strftime('%Y%m%d', time.strptime(x['bdate'], '%d.%m.%Y'))
--- bot/vk/main.py
+++ bot/vk/main.py
@@ -1,5 +1,8 @@
 from func import *
 
-send(140420515, 'хоба работает')
+#send(140420515, 'хоба работает', [])
 
 print(info(140420515))
+
+for i in read():
+	print(i[0], i[1])
