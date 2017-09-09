87f89690d6dac53777bba99d3a3d78b380ebe5f8
Poloz Alexey <polozhev@mail.ru>
2017-09-09 17:20:51 +0300

    All worked perceptron

--- ai/perceptron/main.py
+++ ai/perceptron/main.py
@@ -2,43 +2,40 @@
 import random
 '''
 
-act = lambda xe, we: we[0] + sum([xe[i] * we[i+1] for i in range(len(xe))])
-
-'''
-	s=we[0]
-	for i in range(len(xe)):
-		s += xe[i] * we[i+1]
-'''
+act = lambda xe, we: sum([xe[i] * we[i] for i in range(len(xe))])
 
 yr = [1, 1, 0]
-
 x = [[1, 0.3], [0.4, 0.5], [0.7, 0.8]]
 w = [0 for j in range(len(x)+1)]
 
 '''
 yr = [182, 170, 162, 148]
-
 x1 = [62, 76, 70, 33]
 w1 = [random.random() for j in range(2)]
 '''
 
 for i in range(len(x)):
-	error = (yr[i] - act(x[i], w))
+	x[i]=[1]+x[i]
 
-	print('Δw0 =', error)
-	w[0] += error
+for iteration in range(100):
+	print('Iteration №{}'.format(iteration+1))
 
-	for j in range(len(x[i])):
-		print('Δw%d = %f' % (j+1, x[i][j] * error))
-		w[j+1] += x[i][j] * error
+	for i in range(len(x)):
+		#x[i]=[1]+x[i]
+		error = (yr[i] - act(x[i], w))
 
-	print('-----')
+		for j in range(len(x[i])):
+			print('Δw%d = %f' % (j, x[i][j] * error))
+			w[j] += x[i][j] * error
 
-x=[float(i) for i in input().split()]
-s=w[0]
-for i in range(len(x)):
-	s += x[i] * w[i+1]
-if s>=0.5:
-	print('YES ({}%)'.format(round(s*100)))
-else:
-	print('NO ({}%)'.format(round((1-s)*100)))
+		print('-----')
+
+while True:
+	x = [float(i) for i in input().split()]
+	s = w[0]
+	for i in range(len(x)):
+		s += x[i] * w[i+1]
+	if s>=0.5:
+		print('YES ({}%)'.format(round(s*100)))
+	else:
+		print('NO ({}%)'.format(round((1-s)*100)))
