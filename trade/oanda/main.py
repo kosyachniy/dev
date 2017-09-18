631b22b546635b1570ef25883c0b34d67192a5f5
Poloz Alexey <polozhev@mail.ru>
2017-09-18 17:50:00 +0300

    Worked connect to Oanda

--- trade/oanda/main.py
+++ trade/oanda/main.py
@@ -1,17 +1,17 @@
 import requests, json
+from urllib.parse import urlencode
 
 with open('set.txt', 'r') as file:
     key = json.loads(file.read())
     accountid = key['accountid']
     token = key['token']
-print(accountid, token)
 
 def connect_to_stream():
     try:
         s = requests.Session()
-        url = 'https://stream-fxpractice.oanda.com/v1/prices' #https://api-fxtrade.oanda.com/v1/accounts
+        url = 'https://api-fxpractice.oanda.com/v1/prices'
         headers = {'Authorization' : 'Bearer ' + token} #, 'X-Accept-Datetime-Format' : 'unix'
-        params = {'instruments' : 'EUR_USD,AUD_JPY', 'accountId' : accountid}
+        params = {'instruments' : 'EUR_USD', 'accountId' : accountid} #EUR_USD
         req = requests.Request('GET', url, headers = headers, params = params)
         pre = req.prepare()
         resp = s.send(pre, stream=True, verify=False, timeout=20)
@@ -22,13 +22,38 @@ def connect_to_stream():
 
 def read(response):
     try:
-         for line in response.iter_lines(1):
-             if line:
-                 msg = json.loads(line)         
-                 if msg.has_key("instrument") or msg.has_key("tick"): 
-                       strategy(msg['tick']['instrument'], msg['tick']['time'], msg['tick']['ask'])
+        for line in response.iter_lines(1):
+            if line:
+                msg = json.loads(line)         
+                if msg.has_key("instrument") or msg.has_key("tick"): 
+                    strategy(msg['tick']['instrument'], msg['tick']['time'], msg['tick']['ask'])
     except Exception as e:
-         print("something gone bad " + str(e))
-         return
+        print("something gone bad " + str(e))
+        return
 
-print(connect_to_stream())
+def order(instr, take_profit, stop_loss):
+    try:
+        url = "https://" + "api-fxpractice.oanda.com" + "/v1/accounts/"+accountid+"/orders"
+        
+        headers = {'Authorization' : 'Bearer ' + token,
+                    #'X-Accept-Datetime-Format' : 'unix'
+                    "Content-Type" : "application/x-www-form-urlencoded"
+                  }
+    
+        params = urlencode({
+            "instrument" : instr, #инструмент, по которому открывает сделку
+            "units" : 10, #сколько единиц покупаем
+            "type" : 'market', #прям сейчас исполнить!
+            "side" : "buy", #считаем, что цена пойдет вверх ("sell" если думаем что вниз)
+            "takeProfit" : take_profit, #насколько цена должна пройти вверх, чтобы наша жадность удовлетворилась, и мы закрыли бы сделку. и считали профит
+            "stopLoss" : stop_loss #насколько цена может опуститься, прежде чем наш страх скажет "ты чё?!! дальше только хуже будет. закрывай немедля. фиг с ними с потерями"
+            })
+                                    
+        req = requests.post(url, data=params, headers=headers)
+        for line in req.iter_lines(1):
+            print("order responce: ", line)
+    except Exception as e:
+        print("Caught exception when connecting to orders\n" + str(e))
+
+print(connect_to_stream().json())
+print(order('EUR_USD', 0.0001, 0.001))
