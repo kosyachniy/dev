07bcfc896f271542e1a3e4c6e6ee884feff47923
Poloz Alexey <polozhev@mail.ru>
2017-11-06 23:50:59 +0300

    Get message in user

--- bot/telegram/user/func.py
+++ bot/telegram/user/func.py
@@ -13,14 +13,26 @@ user = client.sign_in(code=input('Код: '))
 '''
 if not client.is_user_authorized():
 	client.send_code_request(x['phone'])
-	user = client.sign_in(x['phone'], input('Код: '))
+	client.sign_in(x['phone'], input('Код: '))
 
 #print(user.stringify())
 #dialogs, entities = client.get_dialogs(10)
 #print(dialogs, entities[0])
 
-for i in client.get_dialogs(10):
+#print(client.get_message_history())
+
+'''
+for i in client.get_message_history(136563129)[1]: #get_message_history #get_input_entity #get_entity
+	print(i.message)
+'''
+
+for i in client.get_dialogs()[0]:
+	x = i.to_dict()['peer']
+	x = [x[i] for i in x][0]
+	print(x)
 	try:
-		print(i.id)
+		for i in client.get_message_history(x)[1]:
+			print(i.message)
 	except:
-		print('No!')
+		pass
+	print('-----')
