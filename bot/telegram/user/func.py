8208673bbd20e24c86dbea8a5389df61fcb4b548
Poloz Alexey <polozhev@mail.ru>
2017-11-06 22:57:57 +0300

    Get variable in Telethon

--- bot/telegram/user/func.py
+++ bot/telegram/user/func.py
@@ -6,9 +6,21 @@ with open('set.txt', 'r') as file:
 
 client = TelegramClient(x['name'], x['id'], x['hash'])
 client.connect()
+
 '''
-client.sign_in(phone=x['telephone'])
-me = client.sign_in(code=input('Код: '))
+client.sign_in(phone=x['phone'])
+user = client.sign_in(code=input('Код: '))
 '''
-client.send_code_request(x['phone'])
-user = client.sign_in(x['phone'], input('Код: '))
+if not client.is_user_authorized():
+	client.send_code_request(x['phone'])
+	user = client.sign_in(x['phone'], input('Код: '))
+
+#print(user.stringify())
+#dialogs, entities = client.get_dialogs(10)
+#print(dialogs, entities[0])
+
+for i in client.get_dialogs(10):
+	try:
+		print(i.id)
+	except:
+		print('No!')
