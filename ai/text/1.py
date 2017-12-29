f1b036e1041f67c6b5e47663ebb75f5dec70fd6f
Poloz Alexey <polozhev@mail.ru>
2017-12-29 03:05:36 +0300

    Worked Word2Vec

--- ai/text/1.py
+++ ai/text/1.py
@@ -1,16 +1,21 @@
+'''
+from gensim.models.keyedvectors import KeyedVectors
+'''
 from gensim.models import KeyedVectors #, Word2Vec
-#from gensim.models.keyedvectors import KeyedVectors
-model = KeyedVectors.load_word2vec_format('ruwikiruscorpora_0_300_20.bin', binary=True)
+model = KeyedVectors.load_word2vec_format('araneum_upos_skipgram_600_2_2017.bin', binary=True)
+
 
 '''
 x = model.wv['мама']
 x = model.most_similar(positive=['мама'], topn=1)
 x = Word2Vec(['мама'], min_count=1, size=1)
+x = model.most_similar(positive=['женщина', 'король'], negative=['мужчина'])
 x = model.wv.most_similar(positive=['женщина', 'король'], negative=['мужчина'])
 x = model.wv.most_similar_cosmul(positive=['woman', 'king'], negative=['man'])
 x = model.wv.doesnt_match("breakfast cereal dinner lunch".split())
 x = model.wv.similarity('woman', 'man')
+x = model.vocab.keys()
 '''
-x = model.most_similar(positive=['женщина', 'король'], negative=['мужчина'])
+x = model.most_similar(positive=['женщина_NOUN', 'король_NOUN'], negative=['мужчина_NOUN'])
 
 print(x)
