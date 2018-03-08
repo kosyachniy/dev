8d236f898b3aedaa1dba8024ac68fd55e139e526
Poloz Alexey <polozhev@mail.ru>
2018-03-09 02:18:46 +0300

    Add template

--- parse/text/2.py
+++
@@ -1,8 +0,0 @@
-char='йцукенгшщзхъёфывапролджэячсмитьбюqwertyuiopasdfghjklzxcvbnm1234567890'
-
-def text(cont):
-	cont=list(cont.lower())
-	for i in range(len(cont)):
-		if cont[i] not in char:
-			cont[i]=' '
-	return ''.join(cont).split()
