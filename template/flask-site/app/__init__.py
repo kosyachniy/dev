ccf3f643983fad98df31b5f7f636cd3c4ad5ec00
Poloz Alexey <polozhev@mail.ru>
2018-06-10 01:57:48 +0300

    Test gzip 2

--- template/flask-site/app/__init__.py
+++ template/flask-site/app/__init__.py
@@ -1,45 +1,13 @@
-from flask import Flask, redirect, after_this_request, request
-#from flask_compress import Compress
-#from cStringIO import StringIO as IO
-from io import BytesIO as IO
-import gzip
-import functools
+from flask import Flask, redirect
+from flask_compress import Compress
 
-def gzipped(f):
-    @functools.wraps(f)
-    def view_func(*args, **kwargs):
-        @after_this_request
-        def zipper(response):
-            accept_encoding = request.headers.get('Accept-Encoding', '')
-
-            if 'gzip' not in accept_encoding.lower():
-                return response
-
-            response.direct_passthrough = False
-
-            if (response.status_code < 200 or
-                response.status_code >= 300 or
-                'Content-Encoding' in response.headers):
-                return response
-            gzip_buffer = IO()
-            gzip_file = gzip.GzipFile(mode='wb', fileobj=gzip_buffer)
-            gzip_file.write(response.data)
-            gzip_file.close()
-
-            response.data = gzip_buffer.getvalue()
-            response.headers['Content-Encoding'] = 'gzip'
-            response.headers['Vary'] = 'Accept-Encoding'
-            response.headers['Content-Length'] = len(response.data)
-
-            return response
-
-        return f(*args, **kwargs)
-
-    return view_func
+compress = Compress()
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 #Compress(app)
+compress.init_app(app)
 
 LINK = 'http://167.99.128.56/'
 
