d2d5ea0a34f0f29965ab1141d316d1e623f13aa0
Poloz Alexey <polozhev@mail.ru>
2018-06-10 01:55:12 +0300

    Test gzip 1

--- template/flask-site/app/__init__.py
+++ template/flask-site/app/__init__.py
@@ -1,7 +1,45 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, after_this_request, request
+#from flask_compress import Compress
+#from cStringIO import StringIO as IO
+from io import BytesIO as IO
+import gzip
+import functools
+
+def gzipped(f):
+    @functools.wraps(f)
+    def view_func(*args, **kwargs):
+        @after_this_request
+        def zipper(response):
+            accept_encoding = request.headers.get('Accept-Encoding', '')
+
+            if 'gzip' not in accept_encoding.lower():
+                return response
+
+            response.direct_passthrough = False
+
+            if (response.status_code < 200 or
+                response.status_code >= 300 or
+                'Content-Encoding' in response.headers):
+                return response
+            gzip_buffer = IO()
+            gzip_file = gzip.GzipFile(mode='wb', fileobj=gzip_buffer)
+            gzip_file.write(response.data)
+            gzip_file.close()
+
+            response.data = gzip_buffer.getvalue()
+            response.headers['Content-Encoding'] = 'gzip'
+            response.headers['Vary'] = 'Accept-Encoding'
+            response.headers['Content-Length'] = len(response.data)
+
+            return response
+
+        return f(*args, **kwargs)
+
+    return view_func
 
 app = Flask(__name__)
 app.config.from_object('config')
+#Compress(app)
 
 LINK = 'http://167.99.128.56/'
 
--- template/flask-site/app/index.py
+++ template/flask-site/app/index.py
@@ -1,11 +1,12 @@
 from flask import render_template, session
-from app import app, LINK
+from app import app, LINK, gzipped
 
 from requests import post
 from json import loads
 
 @app.route('/', methods=['GET'])
 @app.route('/index')
+@gzipped
 def index():
 	categories = loads(post(LINK, json={'method': 'categories.gets'}).text)
 	user = loads(post(LINK, json={'method': 'users.get', 'id': session['id']}).text) if 'id' in session else {'id': 0, 'admin': 2}
