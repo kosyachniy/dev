475f6cf27dad3f7bbe08986c6f87b3920b231c4e
kosyachniy <polozhev@mail.ru>
2018-12-15 03:44:13 +0300

    Serverless Flask worked

--- serverless/now-flask/index.py
+++ serverless/now-flask/index.py
@@ -1,21 +1,13 @@
 from flask import Flask
-from flask_restplus import Resource, Api, fields
-from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
-api = Api(app,
-	version='0.1',
-	title='Our sample API',
-	description='This is our sample API'
-)
 
-@api.route('/hello_world')
-class HelloWorld(Resource):
-	def get(self):
-		return {'hello': 'world'}
+
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, port=3000)
