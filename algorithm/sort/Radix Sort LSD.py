eef3401403c2540a6ff848560f8b4fdccd5d5b90
kosyachniy <polozhev@mail.ru>
2018-12-20 20:26:18 +0300

    Update Radix LSD

--- algorithm/sort/Radix Sort LSD.py
+++ algorithm/sort/Radix Sort LSD.py
@@ -1,58 +1,47 @@
-def counting_sort(collection, d_range, digit=-1):
-    d_range += 1
+from math import log
+ 
 
-    B = list(range(len(collection) + 1))
-    C = list(range(d_range))
+def getDigit(num, base, digit_num):
+    return (num // base ** digit_num) % base  
 
-    for i in range(d_range):
-        C[i] = 0
+def makeBlanks(size):
+    return [ [] for i in range(size) ]  
 
-    for j in collection:
-        C[j[digit]] += 1
+def split(a_list, base, digit_num):
+    buckets = makeBlanks(base)
+    for num in a_list:
+        buckets[getDigit(num, base, digit_num)].append(num)  
 
-    for i in range(1, d_range):
-        C[i] = C[i] + C[i - 1]
+    return buckets
 
-    for i in range(len(collection)-1, -1, -1):
-        B[C[collection[i][digit]]] = collection[i]
-        C[collection[i][digit]] -= 1
+def merge(a_list):
+    new_list = []
+    for sublist in a_list:
+       new_list.extend(sublist)
 
-    return B[1:]
+    return new_list
+ 
+def maxAbs(a_list):
+    return max(abs(num) for num in a_list)
+ 
+def split_by_sign(a_list):
+    buckets = [[], []]
+    for num in a_list:
+        if num < 0:
+            buckets[0].append(num)
+        else:
+            buckets[1].append(num)
 
-def radix_sort(collection, radix):
-    new_collect = []
-    max_len = 0
+    return buckets
+ 
+def radixSort(a_list, base):
+    passes = int(round(log(maxAbs(a_list), base)) + 1) 
+    new_list = list(a_list)
+    for digit_num in range(passes):
+        new_list = merge(split(new_list, base, digit_num))
 
-    for i in collection:
-        num = []
-        while i >= radix:
-            remain = i % radix
-            num.append(remain)
-            i = i // radix
-        num.append(i)
-        new_collect.append(num)
-
-        if len(num) > max_len:
-            max_len = len(num)
-
-    for i in range(0, len(new_collect)):
-        space = max_len - len(new_collect[i])
-        patch = [0 for j in range(space)]
-        new_collect[i].extend(patch)
-
-        new_collect[i].append(i)
-        new_collect[i].reverse()
-
-    for i in range(-1, -1 - max_len, -1):
-        new_collect = counting_sort(new_collect, radix - 1, i)
-
-    return_list = list(range(len(collection)))
-
-    for i in range(0, len(collection)):
-        return_list[i] = collection[new_collect[i][0]]
-
-    return return_list
+    return merge(split_by_sign(new_list))
 
 
 if __name__ == '__main__':
-	print(radix_sort([1234, 56, 7, 77, 23, 15, 16, 16, 6, 222], 8))
+	print(radixSort ([1234, 56, 7, 77, 23, 15, 16, 16, 6, 222], 8))
