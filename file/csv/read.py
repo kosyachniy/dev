590ac9347be10428d3d457ecabb059ae26325e90
kosyachniy <polozhev@mail.ru>
2020-01-14 14:55:39 +0300

    CSV: read data in float

--- file/csv/read.py
+++ file/csv/read.py
@@ -1,8 +1,15 @@
 import csv
 
-def read(name='db', sign=','):
-	with open(name+'.csv', 'r') as file:
-		return [i for i in csv.reader(file, delimiter=sign, quotechar=' ')]
+
+def read_file(name, sign=','):
+	with open('{}.csv'.format(name), 'r') as file:
+		return list(csv.reader(file, delimiter=sign, quotechar=' '))
+
+def read_data(name, skip=1, sign=','):
+	with open('data/{}.csv'.format(name), 'r') as file:
+		data = list(csv.reader(file, delimiter=sign, quotechar=' '))[skip:]
+		return list(map(lambda x: list(map(float, x)), data))
+
 
 if __name__=='__main__':
-	print(read())
+	print(read_file('db'))
