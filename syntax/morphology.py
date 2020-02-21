2583ac1ad4f97efc5f5d9ce9a9a3512eb4f8fd6c
kosyachniy <polozhev@mail.ru>
2020-02-21 14:44:03 +0300

    Morphology: person, tense, animacy, aspect, involvement, mood, transitivity, voice

--- syntax/morphology.py
+++ syntax/morphology.py
@@ -58,11 +58,76 @@ gender = {
 	# ? общий род
 }
 
+person = {
+	None: '-',
+	'1per': 'первое',
+	'2per': 'второе',
+	'3per': 'третье',
+}
+
+tense = {
+	None: '-',
+	'past': 'прошедшее',
+	'pres': 'настоящее',
+	'futr': 'будущее',
+}
+
+animacy = {
+	None: '-',
+	'anim': 'одушевлённое',
+	'inan': 'неодушевлённое',
+}
+
+aspect = {
+	None: '-',
+	'perf': 'совершенный',
+	'impf': 'несовершенный',
+}
+
+involvement = {
+	None: '-',
+	'incl': 'включённый',
+	'excl': 'невключённый',
+}
+
+mood = {
+	None: '-',
+	'impr': 'повелительное',
+	'indc': 'изъявительное',
+}
+
+transitivity = {
+	None: '-',
+	'intr': 'непереходный',
+	'tran': 'переходный',
+}
+
+voice = {
+	None: '-',
+	'pssv': 'страдательный',
+	'actv': 'действительный',
+}
+
 OUTPUT = f'''
 Часть речи: {pos[lemma.tag.POS]}
 Падеж: {case[lemma.tag.case]}
 Число: {number[lemma.tag.number]}
 Род: {gender[lemma.tag.gender]}
+Лицо: {person[lemma.tag.person]}
+Время: {tense[lemma.tag.tense]}
+Одушевлённость: {animacy[lemma.tag.animacy]}
+Вид: {aspect[lemma.tag.aspect]}
+Включённость: {involvement[lemma.tag.involvement]}
+Наклонение: {mood[lemma.tag.mood]}
+Переходность: {transitivity[lemma.tag.transitivity]}
+Залог: {voice[lemma.tag.voice]}
 '''
 
+# Форма: (личная, ...)
+# Разряд: (Личные, Возвратные, Притяжательные, Определительные, Указательные, Вопросительные, Относительные, Неопределенные, Отрицательные)
+
 print(OUTPUT)
+
+# print(dir(lemma.tag))
+# lemma.tag.KNOWN_GRAMMEMES
+# lemma.tag.cyr_repr
