f6dbff7843104a3111a92b387409ae7a9e4a285e
Алексей Полоз <polozhev@mail.ru>
2022-03-13 23:40:11 +0300

    Upload interface for AWS S3

---
+++ aws/Upload.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+import S3 from "react-aws-s3";
+
+function Upload() {
+  const fileInput = useRef();
+  const handleClick = (event) => {
+    event.preventDefault();
+    let file = fileInput.current.files[0];
+    let newFileName = fileInput.current.files[0].name.replace(/\..+$/, "");
+    const config = {
+      bucketName: process.env.REACT_APP_BUCKET_NAME,
+      dirName: process.env.REACT_APP_DIR_NAME /* optional */,
+      region: process.env.REACT_APP_REGION,
+      accessKeyId: process.env.REACT_APP_ACCESS_ID,
+      secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
+    };
+    const ReactS3Client = new S3(config);
+    ReactS3Client.uploadFile(file, newFileName).then((data) => {
+      console.log(data);
+      if (data.status === 204) {
+        console.log("success");
+      } else {
+        console.log("fail");
+      }
+    });
+  };
+
+  return (
+    <>
+      <form className='upload-steps' onSubmit={handleClick}>
+        <label>
+          Upload file:
+          <input type='file' ref={fileInput} />
+        </label>
+        <br />
+        <button type='submit'>Upload</button>
+      </form>
+    </>
+  );
+}
+
+export default Upload;
