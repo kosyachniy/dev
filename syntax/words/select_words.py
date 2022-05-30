e58604662c9b401f7cb085c5ab14bd8385be9c73
kosyachniy <polozhev@mail.ru>
2022-05-30 20:38:41 +0300

    Smart word selection

--- syntax/words/select_words.py
+++ syntax/words/select_words.py
@@ -1,18 +1,23 @@
 import os
+from collections import defaultdict
 
 
 LANG = 'ru'
-NOTIFY_LIMIT = None
 
 
-def combine_files(speech, notify=True, count_letters=None):
-    words = set()
+def combine_files(
+    speech,
+    notify=True,
+    count_letters=None,
+    replace=None,
+    frequency=1,
+):
+    words_files = {}
+    words_counter = defaultdict(int)
 
     for file_name in sorted(os.listdir(f'{LANG}/{speech}/')):
-        count = len(words)
-
         with open(f'{LANG}/{speech}/{file_name}', 'r') as file:
-            included = set()
+            words_files[file_name] = set()
 
             for row in file:
                 word = row.strip().lower()
@@ -23,35 +28,73 @@ def combine_files(speech, notify=True, count_letters=None):
                 if count_letters and len(word) != count_letters:
                     continue
 
-                if word not in words:
-                    words.add(word)
-                    included.add(word)
+                if replace:
+                    for letter_from, letter_to in replace.items():
+                        word = word.replace(letter_from, letter_to)
 
-        if notify:
-            print(f"{file_name}:  +{len(words) - count} words")
+                if word not in words_files[file_name]:
+                    words_files[file_name].add(word)
+                    words_counter[word] += 1
 
-            if count and (not NOTIFY_LIMIT or count < NOTIFY_LIMIT):
-                print(*included, sep=", ")
+    words_once = {i for i in words_counter if words_counter[i] == 1}
+
+    if notify:
+        print(f"BASE:  {len(words_counter) - len(words_once)} words")
+
+        for file_name, words_file in words_files.items():
+            words_included = words_file & words_once
+            print(f"{file_name}:  +{len(words_included)} words")
+            print(*words_included, sep=", ")
+
+    return {word for word, count in words_counter.items() if count >= frequency}
+
+def select_words(
+    speech='nouns',
+    notify=True,
+    count_letters=None,
+    replace=None,
+    frequency=1,
+    exclude=False,
+):
+    words = combine_files(
+        speech,
+        notify=notify,
+        count_letters=count_letters,
+        replace=replace,
+        frequency=frequency,
+    )
+
+    if exclude:
+        exclude = combine_files(
+            'exclude',
+            notify=False,
+            count_letters=count_letters,
+            replace=replace,
+        )
+
+        count = len(words)
+        excluded = set()
+
+        for word in exclude:
+            if word in words:
+                excluded.add(word)
+                words.remove(word)
+
+        print(f"-{count - len(words)} words")
+        print(*excluded, sep=", ")
 
     return words
 
-def select_words(speech='nouns', count_letters=None):
-    words = combine_files(speech, notify=True, count_letters=count_letters)
-    exclude = combine_files(
-        'exclude', notify=False, count_letters=count_letters,
-    )
-
-    count = len(words)
-    excluded = set()
-
-    for word in exclude:
-        if word in words:
-            excluded.add(word)
-            words.remove(word)
-
-    print(f"-{count - len(words)} words")
-    print(*excluded, sep=", ")
-
 
 if __name__ == '__main__':
-    select_words()
+    words = select_words(notify=False)
+    print(f"All: {len(words)}")
+
+    words = select_words(
+        count_letters=5,
+        replace={'ё': 'е'},
+        frequency=1,
+        exclude=True,
+    )
+    print(f"5 letters: {len(words)}")
+    print(*words, sep=", ")
