1cd7693e655caa232bfc5ff8beb18e22e90a88cb
Alex Poloz <alexypoloz@gmail.com>
2022-08-08 11:17:18 +0300

    Static files

--- js/node/app.js
+++ js/node/app.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const cors = require('cors')
-const fs = require("fs");
+const fs = require('fs')
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
-
-app.get('/', function (req, res) {
-    console.log(req)
-    fs.createReadStream('index.html').pipe(res)
+app.use(express.static(__dirname + '/public'))
+app.use((req, res, next) => {
+    console.log('Middleware', req.url, req.headers)
+    next()
 })
 
-app.post('/', function (req, res) {
-    console.log(req.body)
+app.get('/', (req, res) => {
+    // fs.createReadStream('index.html').pipe(res)
+    // fs.readFile('index.html', 'utf8', (err, data) => {
+    //     res.send(data)
+    // })
+    // res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
     res.json({data: req.body})
 })
 
