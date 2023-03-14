d864123cd7168afabc4a41b21caf8112851926d3
Alex Poloz <alexypoloz@gmail.com>
2023-03-14 19:43:39 +0700

    Comments on Midjourney

--- ai/midjourney.py
+++ ai/midjourney.py
@@ -63,13 +63,13 @@ def send_command(data):
         json=payload,
     ).text
 
-def press_button(message_id, data):
+def press_button(message, data):
     payload = {
         "type": 3,
         "guild_id": None,
         "channel_id": CHANNEL,
         "message_flags": 0,
-        "message_id": message_id,
+        "message_id": message,
         "application_id": APPLICATION,
         "session_id": "7687ebdc6846e1f686f02b14c4f150d1",
         "data": {
@@ -101,16 +101,18 @@ def heartbeat(interval, ws):
         send_json_request(ws, heartbeatJSON)
 
 def main():
-    req = input("Request: ")
-    send_command(req)
+    title = input("Request: ")
+    send_command(title)
 
     ws = websocket.WebSocket()
     ws.connect('wss://gateway.discord.gg/?v=6&encording=json')
 
+    # Online
     event = recieve_json_response(ws)
     heartbeat_interval = event['d']['heartbeat_interval'] / 1000
     threading._start_new_thread(heartbeat, (heartbeat_interval, ws))
 
+    # Start session
     payload = {
         'op': 2,
         'd': {
@@ -124,9 +126,12 @@ def main():
     }
     send_json_request(ws, payload)
 
+    # Receiving
     while True:
         try:
             event = recieve_json_response(ws)
+
+            # Check event
             if (
                 not event
                 or event['t'] != 'MESSAGE_CREATE'
@@ -139,25 +144,29 @@ def main():
             ):
                 continue
 
-            title = re.sub(r'.*\*\*([^*]*)\*\*.*', r'\1', event['d']['content'])
-            if title != req:
+            # Check desired image
+            req = re.sub(r'.*\*\*([^*]*)\*\*.*', r'\1', event['d']['content'])
+            if req != title:
                 continue
 
+            # Upscale
             if "Upscaled" not in event['d']['content']:
-                # NOTE: Upscale all 4 image
-                press_button(event['d']['id'], event['d']['components'][0]['components'][0]['custom_id'])
-                press_button(event['d']['id'], event['d']['components'][0]['components'][1]['custom_id'])
-                press_button(event['d']['id'], event['d']['components'][0]['components'][2]['custom_id'])
-                press_button(event['d']['id'], event['d']['components'][0]['components'][3]['custom_id'])
+                message = event['d']['id']
+                buttons = event['d']['components'][0]['components']
+                press_button(message, buttons[0]['custom_id'])
+                # press_button(message, buttons[1]['custom_id'])
+                # press_button(message, buttons[2]['custom_id'])
+                # press_button(message, buttons[3]['custom_id'])
                 continue
 
+            # Result
             url = event['d']['attachments'][0]['url']
             print(f"{title}: {url}")  # Paste your code here
-
             break
 
         except:
             pass
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
