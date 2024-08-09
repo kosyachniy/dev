c92dfb5fbab10a42686cf2a1461ea174b726744f
kosyachniy <alexypoloz@gmail.com>
2024-08-09 22:30:31 +0300

    Optimize tg bot

--- bots/tg/bot/main4.py
+++ bots/tg/bot/main4.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 
-from aiogram import Bot, Dispatcher, F, Router
+from aiogram import Bot, Dispatcher, Router, F
 from aiogram.types import (
     Message,
     CallbackQuery,
@@ -9,47 +9,45 @@ from aiogram.types import (
     InlineKeyboardButton,
 )
 from aiogram.filters import Command
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
 from dotenv import load_dotenv
-from loguru import logger
+from loguru import logger as log
 
 
 load_dotenv()
-token = os.getenv("TOKEN")
+
+bot = Bot(token=os.getenv("TOKEN"))
 dp = Dispatcher()
 router = Router()
-
-
-async def main():
-    bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+dp.include_router(router)
 
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    logger.info("Новое стартовое сообщение")
-    inline_keyboard = InlineKeyboardMarkup(
+    log.info(f"command «start» from {msg.chat.id}")
+    keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text="Press the button", callback_data="button"),
+                InlineKeyboardButton(text="Button", callback_data="button"),
             ]
         ]
     )
-    await msg.reply(text="Test button", reply_markup=inline_keyboard)
+    await msg.reply("Test button", reply_markup=keyboard)
 
 
 @router.message()
 async def message_handler(msg: Message):
-    logger.info(f"Входящее сообщение: {msg.text} от @{msg.from_user.username}")
-    await msg.answer(f"Твой ID: {msg.from_user.id}")
+    log.info(f"message «{msg.text}» from {msg.chat.id}")
+    await msg.answer(f"your id: {msg.chat.id}")
 
 
 @router.callback_query(F.data == "button")
 async def callback_handler(callback: CallbackQuery):
-    await callback.message.answer(f"Hello button")
+    log.info(f"callback «button» from {callback.message.chat.id}")
+    await callback.message.answer("Hello button")
+
+
+async def main():
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
