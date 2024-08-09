c76e0b462e08b8342118d500b1d05bca33e3d903
kosyachniy <alexypoloz@gmail.com>
2024-08-09 22:52:35 +0300

    Minimize tg bot

--- bots/tg/bot/main4.py
+++ bots/tg/bot/main4.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 
-from aiogram import Bot, Dispatcher, Router
+from aiogram import Bot, Dispatcher
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 from dotenv import load_dotenv
@@ -12,33 +12,31 @@ load_dotenv()
 
 bot = Bot(token=os.getenv("TOKEN"))
 dp = Dispatcher()
-router = Router()
-dp.include_router(router)
 
 
-@router.message(Command("start"))
+@dp.message(Command("start"))
 async def start_handler(msg):
     log.info(f"command «start» from {msg.chat.id}")
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text="Button", callback_data="button"),
+                InlineKeyboardButton(text="button", callback_data="button"),
             ],
         ],
     )
     await msg.reply("start menu", reply_markup=keyboard)
 
 
-@router.message()
+@dp.message()
 async def message_handler(msg):
     log.info(f"message «{msg.text}» from {msg.chat.id}")
     await msg.answer(f"your id: {msg.chat.id}")
 
 
-@router.callback_query(lambda callback: callback.data == "button")
-async def callback_handler(callback):
-    log.info(f"callback «{callback.data}» from {callback.message.chat.id}")
-    await callback.message.answer("button menu")
+@dp.callback_query(lambda cb: cb.data == "button")
+async def callback_handler(cb):
+    log.info(f"callback «{cb.data}» from {cb.message.chat.id}")
+    await cb.message.answer("button menu")
 
 
 async def main():
